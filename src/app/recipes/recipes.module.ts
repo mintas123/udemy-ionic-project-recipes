6760c0a669da8d77bd98371fe390e388ddda1ffc
Jakub Mróz <jakub.mroz@akmf.pl>
2020-02-25 09:33:47 +0100

    sync progress offline to remote repo

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,14 +7,24 @@ import { IonicModule } from '@ionic/angular';
 import { RecipesPageRoutingModule } from './recipes-routing.module';
 
 import { RecipesPage } from './recipes.page';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RecipesPage
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    RouterModule.forChild(routes),
     RecipesPageRoutingModule
   ],
-  declarations: [RecipesPage]
+  declarations: [RecipesPage,RecipeItemComponent]
 })
 export class RecipesPageModule {}
