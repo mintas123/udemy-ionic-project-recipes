a63e21975b7fd02dfa9c30c99dbfcc113d50a5c3
Jakub Mróz <jakub.mroz@akmf.pl>
2020-02-26 13:17:37 +0100

    android support, demo -recipes deleted, full project started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-
   {
-    path: 'recipes',
-    children: [
-      {
-        path: '',
-        loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
-      },
-      {
-        path: ':recipeId',
-        loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule),
-
-      }
-    ]
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
   }
 ];
 
